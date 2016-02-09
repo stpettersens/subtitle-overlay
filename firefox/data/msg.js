9da6c43d40e607364b3690d9f7764e89b0fd7abc
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-02-09 15:12:03 +0000

    ...

--- firefox/data/msg.js
+++ firefox/data/msg.js
@@ -1,19 +1,19 @@
 /*
-	Subtitle Overlay.
-	Browser extension to overlay subtitles on a streaming video.
+    Subtitle Overlay.
+    Browser extension to overlay subtitles on a streaming video.
 
-	Copyright 2016 Sam Saint-Pettersen.
+    Copyright 2016 Sam Saint-Pettersen.
     Released under the MIT/X11 License.
 */
 
 //...
 
 function sendMessage(request) {
-	console.log('message -> ' + JSON.stringify(request));
+    console.log('message -> ' + JSON.stringify(request));
 }
 
 function sendMessageTab(request) {
-	sendMessage(request);
+    sendMessage(request);
 }
 
 exports.sendMessage = sendMessage;
